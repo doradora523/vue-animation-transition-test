f92f862bf792f2282a30541bcd0a8648f5d35d59
doradora523 <doradora523@naver.com>
2022-07-16 17:21:07 +0900

    create a project

---
+++ src/views/About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="about">
+    <transition 
+      appear 
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import gsap from 'gsap'
+
+export default {
+  setup() {
+    const beforeEnter = (el) => {
+
+    }
+    const enter = (el) => {
+
+    }
+    return { beforeEnter, enter}
+    }
+}
+</script>
+
+<style>
+  .about {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+
+</style>
